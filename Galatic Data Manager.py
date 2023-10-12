519c575826debc5581aa1abc42166d1aa3b11110
Ajindoraptor <144355051+Ajindoraptor@users.noreply.github.com>
2023-10-12 09:27:12 -0400

    Update and rename README.md to Galatic Data Manager.py

---
+++ Galatic Data Manager.py
@@ -0,0 +1,35 @@
+class Starship:
+    def __init__(self, name, model, role, crew_size):
+        self.name = name
+        self.model = model
+        self.role = role
+        self.crew_size = crew_size
+
+    def get_info(self):
+        return f"Starship: {self.name}\n Model: {self.model}\n Crew Size: {self.crew_size}\n"
+    
+    def to_dict(self):
+        return{"name": self.name, "model": self.model, "role": self.role, "crew_size": self.crew_size}
+    
+class GalacticDataManager:
+    def __init__(self):
+        self.data = {}
+
+    def add_starship(self, starship):
+        self.data[starship.name] = starship.to_dict()
+
+    def display_all_starships(self):
+        for starship_name, starship_data in self.data.items():
+            print(f"{starship_name}")
+            for attribute, value in starship_data.items():
+                print(f" {attribute.capitalize()}: {value}")
+            print("\n")
+
+millennium_falcon = Starship("Millennium Falcon", "YT-1300F", "Freighter", 4)
+star_destroyer = Starship("Star Destroyer", "Imperial I-class", "Destroyer", 37000)
+
+galactic_data_manager = GalacticDataManager()
+galactic_data_manager.add_starship(millennium_falcon)
+galactic_data_manager.add_starship(star_destroyer)
+
+galactic_data_manager.display_all_starships
--- README.md
+++
@@ -1,1 +0,0 @@
-# Galactic-Data-Manager
